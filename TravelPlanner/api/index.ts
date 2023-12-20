7c385ceec2a3007e18ff401cd2f21f6eadb0f5b3
Lukas Van Nuffel <lukasvannuffel02@gmail.com>
2023-12-20 13:39:11 +0100

    MongoDB connection

---
+++ TravelPlanner/api/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import mongoose from "mongoose";
+console.log('TEST123');
+
+if(process.env.MONGO_CONNECTION) {
+mongoose
+    .connect(process.env.MONGO_CONNECTION)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error(error));
+}
+else {
+    throw new Error('No MongoDB connection string');
+}
