0042074a3df947a240abb06ff20ea0dc1754bc0a
Lukas Van Nuffel <lukasvannuffel02@gmail.com>
2023-12-23 21:58:46 +0100

    Update MongoDB connection error message

--- TravelPlanner/api/index.ts
+++ TravelPlanner/api/index.ts
@@ -5,9 +5,9 @@ console.log('TEST123');
 if(process.env.MONGO_CONNECTION) {
 mongoose
     .connect(process.env.MONGO_CONNECTION)
-    .then(() => console.log('Connected to MongoDB'))
+    .then(() => console.log('Connected to MongoDB!'))
     .catch((error) => console.error(error));
 }
 else {
-    throw new Error('No MongoDB connection string');
+    throw new Error('No MongoDB connection string!');
 }
